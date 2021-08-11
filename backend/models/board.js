58cf39e1d4baa4263f7a09bde86684361d39be90
Jaime Castro <jandrescastro97@gmail.com>
2021-08-11 15:54:18 -0500

    update class 11/08/2021

---
+++ backend/models/board.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongosee");
+
+const boardSchema = new mongoose.Schema({
+    userId: { type: mongoose.Schema.ObjectId, ref: "user"},
+    name: String,
+    description: String,
+    taskStatus: String,
+    imageUrl: String,
+    date: { type: Date, default: Date.now },
+});
+
+const board = mongoose.model("board", boardSchema);
+module.exports = board;
---
+++ backend/routes/role.js
@@ -0,0 +1,12 @@
+const { Router } = require("express");
+const express = require("express");
+const router = express.Router();
+const RoleController = require("../controllers/role");
+
+//GET POST PUT DELET
+// http://localhost:3001/api/role/registerRole
+router.post("/registerRole", RoleController.registerRole);
+// http://localhost:3001/api/role/listRole
+router.get("/listRole", RoleController.listRole);
+
+module.exports = router;
