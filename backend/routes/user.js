716c0d55a28bb40656fe686de28b428dc68ca7cc
Jaime Castro <jandrescastro97@gmail.com>
2021-08-17 15:57:02 -0500

    update validateUser and Auth

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const router = express.Router();
 const userController = require("../controllers/user");
+const Auth = require("../middleware/auth");
+const validateUser = require("../middleware/validateUser");
 
 router.post("/registerUser", userController.registerUser);
 // http://localhost:3001/api/user/listUser
-router.get("/listUser/:name?", userController.listUser);
+router.get("/listUser/:name?", Auth, validateUser, userController.listUser);
 
 module.exports = router;
